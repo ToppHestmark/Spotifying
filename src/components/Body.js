0bb02cfd0cce6193f68039001fc4d2b6fdd6369c
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-12-01 21:22:49 +0100

    Pulled data from discover weekly banner to the body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useStateContextValue } from '../context/StateProvider';
 import './Body.css';
 import Header from './Header';
 
 function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useStateContextValue()
+
   return (
     <div className="body">
       <Header spotify={spotify} />
+
+      <div className="body__info">
+        <img 
+          src={discover_weekly?.images[0].url} 
+          alt=""/>
+        <div className="body__infoText">
+          <strong>PLATLISTS</strong>
+          <h2>Discover Weekly</h2>
+          <p> {discover_weekly?.description} </p>
+        </div>
+      </div>
     </div>
   )
 }
